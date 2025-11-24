a7091527e9a38fd9c30f79f6b12bd2af38612ff6
MD <mihirooehs@gmail.com>
2025-11-24 21:26:26 +0400

    fixed input spelling

--- RPS.js
+++ RPS.js
@@ -3,5 +3,30 @@ console.log("Hello World:)")
 function getComputerChoice()
 {
    let a = Math.random();
-   return a;
+    if (a  < 0.3333){
+        return "rock";
+    }
+    else if (a > 0.3333 && a <0.6666)
+        return "paper";
+    else
+        return "scissors";
 }
+
+function getHumanChoice()
+{
+    let uselet = prompt("Enter your choice between (R)ock, (P)aper or(Scissors");
+
+    if (uselet === "R" ||uselet === "r")
+        return "rock"
+    else if (uselet === "S" ||uselet === "s")
+        return "scissors"
+
+    else 
+        return "paper"
+
+}
+
+let PS = 0;
+let CS = 0;
+console.log(getHumanChoice());
+
